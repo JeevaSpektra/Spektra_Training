c88065fcb95dbe1a3f556f5d568a5fd5a8e591aa
JeevaRP759 <102504727+JeevaSpektra@users.noreply.github.com>
2022-09-19 11:28:44 +0530

    Add files via upload

---
+++ Angular_API/ATMSoftware.API/ATMSoftware.API/Controllers/ATMDetailsController.cs
@@ -0,0 +1,36 @@
+﻿
+using ATMSoftware.API.Data;
+using ATMSoftware.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATMAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ATMDetailsController : Controller
+    {
+        private readonly ATMDbContext _ATMDbContext;
+        public ATMDetailsController(ATMDbContext aTMDbContext)
+        {
+            _ATMDbContext = aTMDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllATMDetails()
+        {
+            var details = await _ATMDbContext.AtmDetails.ToListAsync();
+            return Ok(details);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] ATMDetails atmDetailsRequest)
+        {
+            atmDetailsRequest.UserPin = Guid.NewGuid();
+
+            await _ATMDbContext.AtmDetails.AddAsync(atmDetailsRequest); 
+            await _ATMDbContext.SaveChangesAsync();
+            return Ok(atmDetailsRequest);
+        }
+    }
+}
---
+++ Angular_API/ATMSoftware.API/ATMSoftware.API/Data/ATMDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ATMSoftware.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATMSoftware.API.Data
+{
+    public class ATMDbContext : DbContext
+    {
+        public ATMDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<ATMDetails> AtmDetails { get; set; }
+    }
+}
